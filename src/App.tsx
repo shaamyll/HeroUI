80bf38383b84faebbbf003695ae792e7edbebd3b
SirajudeenGhouse <sirajudeenghousebasha@gmail.com>
2025-09-22 12:29:26 +0530

    coment

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { NavbarComponent } from './components/Reusable/NavBar';
 import Users from './Pages/Users';
 import Projects from './Pages/Projects';
 import Auth from './Pages/Auth';
-import DashBoardPage from './Pages/DashBoardPage';
+// import DashBoardPage from './Pages/DashBoardPage';
 import Login from './Pages/Login';
 import CardView from './Pages/CardView';
 import HomePage from './Pages/HomePage';
@@ -25,7 +25,7 @@ function App() {
         <Route path='/projects' element={<Projects />} />
         <Route path='/test' element={<Test/>}/>
         <Route path='/asset' element={<Asset/>}/>
-        <Route path='/dashboard' element={<DashBoardPage />} />
+        {/* <Route path='/dashboard' element={<DashBoardPage />} /> */}
         <Route path='/login' element={<Login />} />
         <Route path='/Footer' element={<footer />} />
         <Route path='/cardView' element={<CardView />} />
